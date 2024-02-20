1a176deb047fe7de291f8449abc4c49b53e46708
armoredchimp <mattkasmir@gmail.com>
2024-02-20 14:13:17 -0800

    init

---
+++ myapp/src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  let name = 'Hello Svelte'
+  let src = 'http://picsum.com/200/200'
+  let string = 'Some <strong>string</strong> with html'
+</script>
+
+
+<img {src} alt="test {name}"/>
+<h1>{name.toLowerCase()}!</h1>
+<p>{@html string}</p>
+<style>
+  h1 {
+    color: red;
+  }
+</style>
